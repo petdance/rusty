237ae61ee0914016e07ec0f68a532ef907d61482
Andy Lester <andy@petdance.com>
2020-06-07 22:37:33 -0500

    Swap in .

--- walk/src/main.rs
+++ walk/src/main.rs
@@ -1,7 +1,7 @@
 use walkdir::WalkDir;
 
 fn main() {
-    for entry in WalkDir::new("target/debug").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
         println!("{}", entry.path().display());
     }
 }
