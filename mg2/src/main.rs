3b319f1d4ecc7d19f94c3bb15301613acea3c9fe
Andy Lester <andy@petdance.com>
2020-06-27 21:50:59 -0500

    Starting new minigrep that will remember hits

---
+++ mg2/src/main.rs
@@ -0,0 +1,21 @@
+
+
+use std::process;
+use std::env;
+
+use minigrep::Config;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else( |err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("App error {}", e);
+        process::exit(1);
+    }
+}
