976b10cea6a5a53b07c40ff037b895792c273c2c
Andy Lester <andy@petdance.com>
2020-06-27 22:32:19 -0500

    I have my new hit object kinda working

--- mg2/src/main.rs
+++ mg2/src/main.rs
@@ -1,21 +1,59 @@
-
+#[allow(unused_variables)]
+#[allow(unused_imports)]
+#[allow(dead_code)]
 
 use std::process;
-use std::env;
+use std::fmt;
+use std::result;
 
-use minigrep::Config;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
 
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+pub struct Hit<'a> {
+    lineno: usize,
+    filename: &'a str,
+}
 
-    let config = Config::new(&args).unwrap_or_else( |err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
 
-    if let Err(e) = minigrep::run(config) {
-        println!("App error {}", e);
-        process::exit(1);
+impl fmt::Display for Hit<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "LINE {}", self.lineno)
     }
+}
+
+
+impl fmt::Debug for Hit<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "DEBUG LINE {}", self.lineno)
+    }
+}
+
+
+fn main() -> io::Result<()> {
+    let filename = "Cargo.lock".to_string();
+
+    let f = match File::open(&filename) {
+        Ok(v) => v,
+        Err(e) => return Err(From::from(e))
+    };
+
+    let f = BufReader::new(f);
+
+    //let mut results = Vec::new();
+    let query = "aut".to_string();
+    let mut lineno = 0;
+    for line in f.lines() {
+        lineno = lineno + 1;
+        let x = line.unwrap();
+        println!("{}: {:?}", lineno, x);
+        if x.contains(&query) {
+            let hit = Hit { lineno: lineno, filename: &filename };
+            println!("HIT {:#?}", hit);
+            println!("HIT {}", hit);
+        }
+    }
+
+    Ok(())
 }
